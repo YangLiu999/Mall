2c20453189f1d30f3479de5b9bba7d87583f8458
yangliu <15998990247@163.com>
2023-10-27 15:25:02 +0800

    gateway集成oauth2第一种方式

--- gateway-service/src/main/java/com/yangliu/gateway/oauth/AccessManager.java
+++ gateway-service/src/main/java/com/yangliu/gateway/oauth/AccessManager.java
@@ -1,68 +1,68 @@
-//package com.yangliu.gateway.oauth;
-//
-//import org.apache.commons.lang3.StringUtils;
-//import org.springframework.security.authorization.AuthorizationDecision;
-//import org.springframework.security.authorization.ReactiveAuthorizationManager;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.oauth2.provider.OAuth2Authentication;
-//import org.springframework.security.web.server.authorization.AuthorizationContext;
-//import org.springframework.stereotype.Component;
-//import org.springframework.util.AntPathMatcher;
-//import org.springframework.web.server.ServerWebExchange;
-//import reactor.core.publisher.Mono;
-//
-//import java.util.Set;
-//import java.util.concurrent.ConcurrentSkipListSet;
-//
-///**
-// * @author YL
-// * @date 2023/08/22
-// **/
-//@Component
-//public class AccessManager implements ReactiveAuthorizationManager<AuthorizationContext> {//响应式
-//
-//    //存放不需要进行token校验的路径（正则表达式）
-//    private  Set<String> permitAll = new ConcurrentSkipListSet<>();
-//    //正则校验器
-//    private static final AntPathMatcher antPathMatcher = new AntPathMatcher();
-//
-//    public AccessManager(){
-//        //可以放行的路径
-//        permitAll.add("/**/oauth/**");
-//    }
-//
-//    //webFlux Mono Flux
-//    @Override
-//    public Mono<AuthorizationDecision> check(Mono<Authentication> authentication,
-//
-//                                             AuthorizationContext authorizationContext) {
-//        //校验核心代码
-//        ServerWebExchange exchange = authorizationContext.getExchange();
-//        return authentication.map(auth -> {
-//           String requestPath = exchange.getRequest().getURI().getPath();
-//           //放行URL校验
-//           if (checkPermitAll(requestPath)){
-//                return new AuthorizationDecision(true);
-//           }
-//           //判断是否是OAuth2Authentication类型
-//           if (auth instanceof OAuth2Authentication){
-//               OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) auth;
-//               String clientId = oAuth2Authentication.getOAuth2Request().getClientId();
-//               if (StringUtils.isNotEmpty(clientId)){
-//                   return new AuthorizationDecision(true);
-//               }
-//           }
-//           return new AuthorizationDecision(false);
-//        });
-//    }
-//
-//    /**
-//     * 校验请求路径是否放行
-//     * @param requestPath
-//     * @return
-//     */
-//    private boolean checkPermitAll(String requestPath) {
-//        return permitAll.stream().filter(p -> antPathMatcher.match(p,requestPath))
-//                .findFirst().isPresent();
-//    }
-//}
+package com.yangliu.gateway.oauth;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.ReactiveAuthorizationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+/**
+ * @author YL
+ * @date 2023/08/22
+ **/
+@Component
+public class AccessManager implements ReactiveAuthorizationManager<AuthorizationContext> {//响应式
+
+    //存放不需要进行token校验的路径（正则表达式）
+    private  Set<String> permitAll = new ConcurrentSkipListSet<>();
+    //正则校验器
+    private static final AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+    public AccessManager(){
+        //可以放行的路径
+        permitAll.add("/**/oauth/**");
+    }
+
+    //webFlux Mono Flux
+    @Override
+    public Mono<AuthorizationDecision> check(Mono<Authentication> authentication,
+
+                                             AuthorizationContext authorizationContext) {
+        //校验核心代码
+        ServerWebExchange exchange = authorizationContext.getExchange();
+        return authentication.map(auth -> {
+           String requestPath = exchange.getRequest().getURI().getPath();
+           //放行URL校验
+           if (checkPermitAll(requestPath)){
+                return new AuthorizationDecision(true);
+           }
+           //判断是否是OAuth2Authentication类型
+           if (auth instanceof OAuth2Authentication){
+               OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) auth;
+               String clientId = oAuth2Authentication.getOAuth2Request().getClientId();
+               if (StringUtils.isNotEmpty(clientId)){
+                   return new AuthorizationDecision(true);
+               }
+           }
+           return new AuthorizationDecision(false);
+        });
+    }
+
+    /**
+     * 校验请求路径是否放行
+     * @param requestPath
+     * @return
+     */
+    private boolean checkPermitAll(String requestPath) {
+        return permitAll.stream().filter(p -> antPathMatcher.match(p,requestPath))
+                .findFirst().isPresent();
+    }
+}
--- gateway-service/src/main/java/com/yangliu/gateway/oauth/SecurityConfig.java
+++ gateway-service/src/main/java/com/yangliu/gateway/oauth/SecurityConfig.java
@@ -1,53 +1,53 @@
-//package com.yangliu.gateway.oauth;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.authentication.ReactiveAuthenticationManager;
-//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-//import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
-//import org.springframework.security.config.web.server.ServerHttpSecurity;
-//import org.springframework.security.oauth2.server.resource.web.server.ServerBearerTokenAuthenticationConverter;
-//import org.springframework.security.web.server.SecurityWebFilterChain;
-//import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
-//
-//import javax.annotation.Resource;
-//import javax.sql.DataSource;
-//
-///**
-// * @author YL
-// * @date 2023/09/13
-// **/
-//@Configuration
-//@EnableWebFluxSecurity
-//public class SecurityConfig {
-//
-//    @Resource
-//    private DataSource dataSource;
-//    @Resource
-//    private AccessManager accessManager;
-//
-//    @Bean
-//    SecurityWebFilterChain webFluxSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
-//
-//        //reactiveAuthenticationManager权限管理器需要底层DB资源管理类dataSource
-//        ReactiveAuthenticationManager reactiveAuthenticationManager =
-//                new ReactiveJdbcAuthenticationManager(dataSource);
-//
-//        //authenticationWebFilter需要必要组件reactiveAuthenticationManager响应式权限管理器
-//        AuthenticationWebFilter authenticationWebFilter =
-//                new AuthenticationWebFilter(reactiveAuthenticationManager);
-//        authenticationWebFilter.
-//                setServerAuthenticationConverter(new ServerBearerTokenAuthenticationConverter());
-//
-//        serverHttpSecurity.httpBasic().disable()
-//                .csrf().disable()
-//                .authorizeExchange()
-//                //每次前端使用ajax调用后台接口回先发送HttpMethod.OPTIONS，所以需要放行
-//                .pathMatchers(HttpMethod.OPTIONS).permitAll()
-//                .anyExchange().access(accessManager)
-//                .and().addFilterAt(authenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION);
-//
-//        return serverHttpSecurity.build();
-//    }
-//}
+package com.yangliu.gateway.oauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.web.server.ServerBearerTokenAuthenticationConverter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+/**
+ * @author YL
+ * @date 2023/09/13
+ **/
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Resource
+    private DataSource dataSource;
+    @Resource
+    private AccessManager accessManager;
+
+    @Bean
+    SecurityWebFilterChain webFluxSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
+
+        //reactiveAuthenticationManager权限管理器需要底层DB资源管理类dataSource
+        ReactiveAuthenticationManager reactiveAuthenticationManager =
+                new ReactiveJdbcAuthenticationManager(dataSource);
+
+        //authenticationWebFilter需要必要组件reactiveAuthenticationManager响应式权限管理器
+        AuthenticationWebFilter authenticationWebFilter =
+                new AuthenticationWebFilter(reactiveAuthenticationManager);
+        authenticationWebFilter.
+                setServerAuthenticationConverter(new ServerBearerTokenAuthenticationConverter());
+
+        serverHttpSecurity.httpBasic().disable()
+                .csrf().disable()
+                .authorizeExchange()
+                //每次前端使用ajax调用后台接口回先发送HttpMethod.OPTIONS，所以需要放行
+                .pathMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyExchange().access(accessManager)
+                .and().addFilterAt(authenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION);
+
+        return serverHttpSecurity.build();
+    }
+}
